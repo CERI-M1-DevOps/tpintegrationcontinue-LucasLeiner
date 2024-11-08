db88af051ee90e1c1609616e42a81e6d928aa0b6
LucasLeiner <lucasleiner05@gmail.com>
2024-11-08 17:24:51 +0100

    Write documentation

--- src/main/java/liste/ListeSimple.java
+++ src/main/java/liste/ListeSimple.java
@@ -1,18 +1,37 @@
 package liste;
 
+/**
+ * A simple linked list implementation where each element points to the next element in the list.
+ */
 public class ListeSimple {
     private long size;
     Noeud tete;
 
+    /**
+     * Returns the size of the list.
+     *
+     * @return the number of elements in the list
+     */
     public long getSize() {
         return size;
     }
 
+    /**
+     * Adds a new element to the front of the list.
+     *
+     * @param element the element to be added to the list
+     */
     public void ajout(int element) {
         tete = new Noeud(element, tete);
         size++;
     }
 
+    /**
+     * Modifies the element of the first node found in the list that matches the specified element.
+     *
+     * @param element the element to be searched for in the list
+     * @param nouvelleValeur the new value to set for the found first node's element
+     */
     public void modifiePremier(Object element, Object nouvelleValeur) {
         Noeud courant = tete;
         while (courant != null && courant.getElement() != element)
@@ -21,6 +40,12 @@ public class ListeSimple {
             courant.setElement(nouvelleValeur);
     }
 
+    /**
+     * Modifies all nodes in the list that have an element matching the specified element with a new value.
+     *
+     * @param element the element to be searched for in the list
+     * @param nouvelleValeur the new value to set for the found nodes' elements
+     */
     public void modifieTous(Object element, Object nouvelleValeur) {
         Noeud courant = tete;
         while (courant != null) {
@@ -30,6 +55,12 @@ public class ListeSimple {
         }
     }
 
+    /**
+     * Returns a string representation of the list.
+     * The string representation consists of list elements enclosed in parentheses.
+     *
+     * @return a string representation of the list in the format "ListeSimple(element1, element2, ...)"
+     */
     public String toString() {
         StringBuilder sb = new StringBuilder("ListeSimple(");
         Noeud n = tete;
@@ -43,6 +74,11 @@ public class ListeSimple {
         return sb.toString();
     }
 
+    /**
+     * Removes the first occurrence of the specified element from the list.
+     *
+     * @param element the element to be removed from the list
+     */
     public void supprimePremier(Object element) {
         if (tete != null) {
             if (tete.getElement() == element) {
@@ -63,6 +99,11 @@ public class ListeSimple {
         }
     }
 
+    /**
+     * Removes all occurrences of the specified element from the list.
+     *
+     * @param element the element to be removed from the list
+     */
     public void supprimeTous(int element) {
        tete = supprimeTousRecurs(element, tete);
     }
@@ -80,6 +121,11 @@ public class ListeSimple {
         } else return null;
     }
 
+    /**
+     * Retrieves the node that is just before the last node in the list.
+     *
+     * @return the node before the last node in the list, or null if the list is empty or has only one node
+     */
     public Noeud getAvantDernier() {
         if (tete == null || tete.getSuivant() == null)
             return null;
@@ -94,6 +140,13 @@ public class ListeSimple {
         }
     }
 
+    /**
+     * Reverses the linked list.
+     *
+     * This method transforms the current linked list such that the head of the list becomes the tail,
+     * and the tail becomes the head. It iterates through the list, reversing the direction of the
+     * "suivant" pointers for each node until the entire list is reversed.
+     */
     public void inverser() {
         Noeud precedent = null;
         Noeud courant = tete;
@@ -106,6 +159,12 @@ public class ListeSimple {
         tete = precedent;
     }
 
+    /**
+     * Retrieves the node that precedes the specified node {@code r} in the list.
+     *
+     * @param r the node whose predecessor is to be found
+     * @return the node that precedes {@code r} in the list
+     */
     public Noeud getPrecedent(Noeud r) {
     // la liste n'est pas vide puisqu'on transmet un Node de la liste et le Node existe obligatoirement
         Noeud precedent = tete;
@@ -117,6 +176,12 @@ public class ListeSimple {
         return precedent;
     }
 
+    /**
+     * Swaps the positions of two specified nodes in the linked list.
+     *
+     * @param r1 the first node to be swapped
+     * @param r2 the second node to be swapped
+     */
     public void echanger(Noeud r1, Noeud r2) {
         if (r1 == r2)
             return;
